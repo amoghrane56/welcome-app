540e8094d1fc1d24c2951bfdba4edaef9aa7389c
Amogh Rane <132093071+amoghrane56@users.noreply.github.com>
2023-12-11 18:06:52 +0530

    Update WelcomeWebScript.py

--- WelcomeWebScript.py
+++ WelcomeWebScript.py
@@ -8,7 +8,7 @@ from docx.shared import Pt
 def fetch_data_from_database(pms_account_code):
     # Establish a connection to the SQL Server database
     connection_string = """DRIVER={ODBC Driver 17 for SQL Server};SERVER=192.168.13.10;
-                          DATABASE=IntegraUAT;UID=amog;PWD=Abcd#123"""
+                          DATABASE=IntegraUAT;UID=amog;PWD=Abcd#123;LoginTimeout=30"""
     conn = pyodbc.connect(connection_string)
     cursor = conn.cursor()
 
